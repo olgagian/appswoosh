e08a0dedeb59fa281d662da30c9fab6419e2637c
olgagian <olgagian@otenet.gr>
2018-03-20 17:43:05 +0200

    Initial Commit

---
+++ appswoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appswoosh
+//
+//  Created by ioannis giannakidis on 20/03/2018.
+//  Copyright © 2018 ioannis giannakidis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
